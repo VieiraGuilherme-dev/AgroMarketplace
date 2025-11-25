c0f809422b136d4a40baa26a599fdda2d47832d4
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-25 08:43:31 -0300

    create Usuario entity

---
+++ src/main/java/com/agro/marketplace/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.agro.marketplace.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
