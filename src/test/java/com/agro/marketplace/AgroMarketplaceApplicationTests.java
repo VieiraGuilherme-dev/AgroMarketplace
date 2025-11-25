7aed2ec9b51a63e6800d4b3003a7e76165740f57
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-25 08:27:51 -0300

    Projeto Criado

---
+++ src/test/java/com/agro/marketplace/AgroMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
