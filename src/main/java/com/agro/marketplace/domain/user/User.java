e439095679b8257c8831d939c76d093bb3de681d
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-26 12:49:51 -0300

    add UserRepository interface

--- src/main/java/com/agro/marketplace/domain/user/User.java
+++ src/main/java/com/agro/marketplace/domain/user/User.java
@@ -1,9 +1,7 @@
 package com.agro.marketplace.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
