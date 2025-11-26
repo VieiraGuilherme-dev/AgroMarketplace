f187aa38b1763ad7a8c2c36268d2a7ee9f85d830
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-26 14:38:58 -0300

    The class EspacoFisico is created and EspacoFisicoRepository is created.

---
+++ src/main/java/com/agro/marketplace/entities/EspacoFisico.java
@@ -0,0 +1,60 @@
+package com.agro.marketplace.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "espaco_fisico")
+public class EspacoFisico {
+
+    @Id
+    private Long id;
+
+    private String nome;
+    private String local;
+    private Integer capacidade;
+
+    public EspacoFisico() {
+
+    }
+
+    public EspacoFisico(Long id, String nome, String local, Integer capacidade) {
+        this.id = id;
+        this.nome = nome;
+        this.local = local;
+        this.capacidade = capacidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+}
---
+++ src/main/java/com/agro/marketplace/repositories/EspacoFisicoRepository.java
@@ -0,0 +1,4 @@
+package com.agro.marketplace.repositories;
+
+public interface EspacoFisicoRepository{
+}
