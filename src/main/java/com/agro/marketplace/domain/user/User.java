b50e206ffea4330bc47a11f5cfb9fb2252939c30
VieiraGuilherme-dev <gv524003@gmail.com>
2025-11-26 12:43:27 -0300

    User entity with basic fields

---
+++ src/main/java/com/agro/marketplace/domain/user/User.java
@@ -0,0 +1,26 @@
+package com.agro.marketplace.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+    private String password;
+
+    private Role role;
+
+}
